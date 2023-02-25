f5f0243bcd9492fbf0755a54ce2dad02eb4f9bf2
rania <contactraniacherif@gmail.com>
2023-02-25 13:32:38 +0100

    added: code Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import TopBar from "./pages/navbar/TopBar";
+import SideBar from "./pages/navbar/SideBar";
 function App() {
-  return <TopBar/>
+  return (
+  <React.Fragment>
+    <TopBar />
+    <SideBar/>
+  </React.Fragment>);
 }
 
 export default App;
