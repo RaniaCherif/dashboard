365c7ad13b17efcb06fbc09c1ccca41e539b0d1d
rania <contactraniacherif@gmail.com>
2023-02-25 14:32:51 +0100

    fixed: erreurs in SideBar.jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import TopBar from "./pages/navbar/TopBar";
 import SideBar from "./pages/navbar/SideBar";
-function App() {
-  return 
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <TopBar />
+        <SideBar></SideBar>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
--- src/pages/navbar/SideBar.jsx
+++ src/pages/navbar/SideBar.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
-import "react-pro-sidebar/dist/css/styles.css";
 import { tokens } from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
@@ -61,7 +60,7 @@ const SideBar = () => {
         },
       }}
     >
-      <ProSidebar collapsed={isCollapsed}>
+      <Sidebar collapsed={isCollapsed}>
         <Menu iconShape="square">
           {/* LOGO AND MENU ICON */}
           <MenuItem
@@ -220,7 +219,7 @@ const SideBar = () => {
             />
           </Box>
         </Menu>
-      </ProSidebar>
+      </Sidebar>
     </Box>
   );
 };
