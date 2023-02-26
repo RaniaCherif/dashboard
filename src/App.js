a9297fe790e0eed82a575481f53dfee28cfd4b84
rania <contactraniacherif@gmail.com>
2023-02-26 05:32:48 +0100

    added: SideBar tsx v

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import TopBar from "./pages/navbar/TopBar";
-import SideBar from "./pages/navbar/SideBar";
+import SideBar from "./pages/navbar/SideBar.tsx";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ProSidebarProvider } from "react-pro-sidebar";
+import { useState } from "react";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <ProSidebarProvider>
-          <CssBaseline />
-          <TopBar />
-          <SideBar></SideBar>
+          <div className="app">
+            <SideBar />
+            <main className="content">
+              <TopBar setIsSidebar={setIsSidebar} />
+            </main>
+          </div>
         </ProSidebarProvider>
       </ThemeProvider>
     </ColorModeContext.Provider>
