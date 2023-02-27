d510ed2b6cf86a1eb01616481042557d36f4b4db
rania <contactraniacherif@gmail.com>
2023-02-27 15:43:18 +0100

    fixed: style of container1

--- src/pages/dashboard/containers/Container1.jsx
+++ src/pages/dashboard/containers/Container1.jsx
@@ -6,23 +6,25 @@ const Container1 = () => {
   return (
     <Box
       className="Container_Base Container_Line"
-      width={"240px"}
-      height={"110px"}
+      width={"200px"}
+      height={"85px"}
     >
       <Box
         display={"flex"}
         flexDirection={"column"}
         flexWrap={"nowrap"}
+        alignItems={"flex-start"}
         width={"120px"}
         height={"70px"}
         marginTop={"10px"}
+        
       >
         <h2>Revenue</h2>
         <h1>
           $52850 <span>+5.8</span>
         </h1>
 
-        <h5>12% since last month</h5>
+        <h5 style={ {margin:0}}>12% since last month</h5>
       </Box>
 
       <DiamondOutlinedIcon
